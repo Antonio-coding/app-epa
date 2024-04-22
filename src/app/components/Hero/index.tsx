bf56df184ba7fb3d182cb4d89973fcd507b017fe
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2024-04-22 10:49:02 -0300

    fix home

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -3,8 +3,8 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import ConectionImg1 from "@/public/images/h1.png";
-import GirlImg2 from "@/public/images/h2.png";
+import ConectionImg1 from "../../../../public/images/h1.png";
+import GirlImg2 from "../../../../public/images/h2.png";
 import Image from 'next/image';
 
 const responsive = {
@@ -27,15 +27,15 @@ const responsive = {
 
 const Hero = () => {
   return (
-    <Carousel 
-    additionalTransfrom={0}
-    arrows={false}
-    autoPlay={true}
-    autoPlaySpeed={4000}
-    centerMode={false}
-    infinite responsive={responsive}
-    itemClass="item"
-    showDots={true}
+    <Carousel
+      additionalTransfrom={0}
+      arrows={false}
+      autoPlay={true}
+      autoPlaySpeed={4000}
+      centerMode={false}
+      infinite responsive={responsive}
+      itemClass="item"
+      showDots={true}
     >
       {/*1st hero slide */}
       <div className="w-[100%] h-[88vh] flex items-center justify-center flex-col bg-primary-blue md:clip_path">
@@ -48,12 +48,12 @@ const Hero = () => {
               Ensine por amor!
             </h1>
             <h1 className="text-[90px] leading-[5rem] uppercase text-white font-bold">
-              Comunidade <br/>EPA!
+              Comunidade <br />EPA!
             </h1>
             <p className="mt-[2rem] text-opacity-70 text-[18px]">
-            Conectando pessoas que transformam o futuro, o Epa promove o ensino por amor, 
-            fortalecendo laços e acessibilidade educacional para todos. Junte-se a nós e
-            faça <br/> parte dessa transformação!
+              Conectando pessoas que transformam o futuro, o Epa promove o ensino por amor,
+              fortalecendo laços e acessibilidade educacional para todos. Junte-se a nós e
+              faça <br /> parte dessa transformação!
             </p>
             <button className="px-4 mt-[1rem] py-3 sm:px-6 sm:py-3 text-[20px] bg-blue-950 transition-all duration-200 hover:bg-red-700 flex items-center rounded-md space-x-2 text-white">
               <span className="font-bold text-base">Comece agora!</span>
@@ -67,19 +67,19 @@ const Hero = () => {
       <div className="w-[100%] h-[88vh] flex items-center justify-center flex-col bg-blue-950 md:clip_path">
         <div className="w-[70%] grid grid-cols-1 md:grid-cols-2 justify-center items-center gap-[2rem] mx-auto">
           {/* Image */}
-          <Image src={GirlImg2} alt="girl" className="hidden md:block"/>
+          <Image src={GirlImg2} alt="girl" className="hidden md:block" />
           {/* information*/}
           <div>
             <h1 className="text-[40px] font-semibold text-primary-blue">
               Aprenda por amor!
             </h1>
             <h1 className="text-[90px] leading-[5rem] uppercase text-white font-bold">
-              Comunidade <br/>EPA!
+              Comunidade <br />EPA!
             </h1>
             <p className="mt-[2rem]  text-white text-[18px]">
-            Conectando pessoas que transformam o futuro, o Epa promove o ensino por amor, 
-            fortalecendo laços e acessibilidade educacional para todos. Junte-se <br/>a nós e
-            faça parte dessa transformação!
+              Conectando pessoas que transformam o futuro, o Epa promove o ensino por amor,
+              fortalecendo laços e acessibilidade educacional para todos. Junte-se <br />a nós e
+              faça parte dessa transformação!
             </p>
             <button className="px-4 mt-[1rem] py-3 sm:px-6 sm:py-3 text-[20px] bg-primary-blue transition-all duration-200 hover:bg-red-700 flex items-center rounded-md space-x-2 text-white">
               <span className="font-bold text-base">Comece agora!</span>
--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import Hero from './Hero/Hero'
-import Feature from './Features/Feature'
+import Hero from './Hero'
+import Feature from './Features'
 import Depositions from './Depositions/Depositions'
-import Newsletter from './Newsletter/Newsletter'
+import Newsletter from './Newsletter'
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
-     <Hero />
-     <Feature/>
-     <Depositions />
-     <Newsletter />
-     
-      </div>
+      <Hero />
+      <Feature />
+      <Depositions />
+      <Newsletter />
+
+    </div>
   )
 }
 
