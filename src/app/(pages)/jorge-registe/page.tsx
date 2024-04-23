595100b8a8181055c64dbc2ac09aa9cec2669d85
JorgeKlisyman <132587757+JorgeKlisyman@users.noreply.github.com>
2024-04-23 22:48:26 +0000

    ajustar o que faltava no registe

---
+++ src/app/(pages)/jorge-registe/page.tsx
@@ -0,0 +1,9 @@
+export default function JorgeRegiste() {
+    return (
+        <>
+            <div className=" w-12 h-10 border  bg-green-600 ">isso é teste</div>
+            <div className=" w-12 h-10 border  bg-red-900 ">isso é teste</div>
+            <div>isso é teste</div>
+        </>
+        )
+}
