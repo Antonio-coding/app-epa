c2ce45bebb31fa8d35aa4ee99b3c0649d1b5d082
Mirla Vieira de Oliveira <mirla.v.oliveira@gmail.com>
2024-04-25 16:41:52 -0300

    ajustes

--- src/app/(pages)/sobre/page.tsx
+++ src/app/(pages)/sobre/page.tsx
@@ -7,7 +7,7 @@ export default function Sobre() {
             <p className="mt-10 px-12 text-blue-950 font-inter text-[24px]">
             O Ensino por Amor surgiu da necessidade de oferecer conhecimento 
             para pessoas que precisam, aulas como matemática, português entre outras, em grupo ou individual,
-            online ou presencial.Nosso jeito de amar é distrubuindo conhecimento.
+            online ou presencial. Nosso jeito de amar é distrubuindo conhecimento.
             Somos a BUG HUNTERS, uma equipe do curso de Sistemas para Internet, idealizamos esse projeto a 
             partir da necessidade querer dar aula para ensinar e passar conhecimento e não saber como fazer.
             </p>
