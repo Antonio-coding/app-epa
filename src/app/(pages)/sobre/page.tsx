7602a407959c8bf5f43659f5ceb275f589e2a084
Mirla Vieira de Oliveira <mirla.v.oliveira@gmail.com>
2024-04-25 16:39:51 -0300

    paragrafo sobre

--- src/app/(pages)/sobre/page.tsx
+++ src/app/(pages)/sobre/page.tsx
@@ -1,8 +1,20 @@
 export default function Sobre() {
     return <>
         <div>
-            <h1 className=" mt-10 bg-primary-blue text-center text-[50px]">Sobre</h1>
+            <h1 className=" mt-10 text-blue-950 font-inter text-center text-[48px] font-semibold">Sobre</h1>
         </div>
+        <div>
+            <p className="mt-10 px-12 text-blue-950 font-inter text-[24px]">
+            O Ensino por Amor surgiu da necessidade de oferecer conhecimento 
+            para pessoas que precisam, aulas como matemática, português entre outras, em grupo ou individual,
+            online ou presencial.Nosso jeito de amar é distrubuindo conhecimento.
+            Somos a BUG HUNTERS, uma equipe do curso de Sistemas para Internet, idealizamos esse projeto a 
+            partir da necessidade querer dar aula para ensinar e passar conhecimento e não saber como fazer.
+            </p>
+            
+        </div>
+
+        
        
     </>
 }
