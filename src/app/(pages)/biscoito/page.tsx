1e05e2f3d030e03448ee8287577f54f7d4af1747
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2024-05-07 13:26:01 -0300

    clean project

--- src/app/(pages)/biscoito/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Biscoito() {
-    return (
-        <>
-            <div className=" w-12 h-10 border  bg-green-600 ">Biscoito</div>
-            <div className=" w-12 h-10 border  bg-red-900 ">Biscoito</div>
-            <div>Biscoito</div>
-        </>
-        )
-}
