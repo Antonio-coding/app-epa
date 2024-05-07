ab9109e6defe6bb4288dfa08019a0b6648fa1a47
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2024-05-07 13:09:35 -0300

    exemplo para Breno deletar depois

---
+++ src/app/(pages)/esqueci-senha/page.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+export default function EsqueciSenha() {
+    return (
+        <>
+            <div>oi Breno</div>
+            <div>oi Breno</div>
+            <div>oi Breno</div>
+            <div>oi Breno</div>
+            <div>oi Breno</div>
+            <div>oi Breno</div>
+            <Link href="/login" className='text-lg border w-[500px] bg-slate-500 hover:text-red-500'>
+                Voltar para o Login
+            </Link>
+        </>
+    )
+}
