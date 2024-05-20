a104cbbb79fd220400870555033da75a1bfa3420
breno-fernandes1 <breno.fernandesbrito@outlook.com>
2024-05-20 16:06:48 -0300

    .

--- app/(pages)/(private-routes)/layout.tsx
+++ app/(pages)/(private-routes)/layout.tsx
@@ -1,29 +1,21 @@
 import type { Metadata } from "next";
 import "@/globals.css";
-
-
 import NavbarFooter from "@/components/NavbarFooter";
 import UserNavbar from "@/components/UserNavbar";
-
-
-
 export const metadata: Metadata = {
-    title: "Epa! | Ensino por amor!",
-    description: "Perfil e dashborad do usuario",
+  title: "Epa! | Ensino por amor!",
+  description: "Perfil e dashborad do usuario",
 };
-
 export default function PrivateLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-
-        <>
-            <UserNavbar />
-            <section>{children}</section>
-            <NavbarFooter />
-        </>
-
-    );
+  return (
+    <>
+      <UserNavbar />
+      <section>{children}</section>
+      <NavbarFooter />
+    </>
+  );
 }
