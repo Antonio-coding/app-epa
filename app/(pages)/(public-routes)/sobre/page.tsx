3d523e851d5dacaa12f48253c651256fb5e98d07
Antônio Thiago Neto <112974950+Antonio-coding@users.noreply.github.com>
2024-05-20 19:38:17 -0300

    fix imgpath

--- app/(pages)/(public-routes)/sobre/page.tsx
+++ app/(pages)/(public-routes)/sobre/page.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { AuthorCard } from "@/components/AuthorCard";
+import ImgPath from '@/constants/imgPath';
+import Image from "next/image";
 // Define uma interface para os autores
 interface Author {
   name: string;
-  img: string;
+  img: string
   linkedin: string;
 }
+
 
 // Lista de autores
 const authors: Author[] = [
   {
     name: "Caio Roberto",
-    img: "/images/imgCaio.png",
+    img: ImgPath.ImgCaio,
     linkedin: "https://www.linkedin.com/in/caio-roberto-187983256/",
   },
   {
     name: "Antonio Thiago",
-    img: "/images/imgThiago.png",
+    img: ImgPath.ImgThiago,
     linkedin: "https://www.linkedin.com/in/ant%C3%B4nio-thiago-b4694524b/",
   },
   {
     name: "Breno Fernandes",
-    img: "/images/imgBreno.jpg",
+    img: ImgPath.ImgBreno,
     linkedin: "https://www.linkedin.com/in/breno-fernandes-46b058238/",
   },
   {
@@ -42,15 +45,17 @@ const AuthorsPage: React.FC = () => {
     <>
       <div className="w-full h-[10rem] flex items-center justify-center bg-primary-blue md:clip_path relative">
         <div className="absolute top-0 mt-0 flex flex-col items-center">
-          <img
+          <Image
+            width={500}
+            height={500}
             className="h-32 w-32 object-contain mb-2"
-            src="/images/logo_login.svg"
+            src={ImgPath.LogoBGBlue}
             alt="Logo"
           />
         </div>
       </div>
-      <div> 
- 
+      <div>
+
         <h2 className=" justify-center text-2xl font-semibold text-center text-black  m-10">
           O EPA surge da necessidade de oferecer conhecimento para pessoas que
           precisam. Aulas como matemática, português etc, em grupo ou
--- app/constants/imgPath/index.tsx
+++ app/constants/imgPath/index.tsx
@@ -2,6 +2,7 @@ const ImgPath = {
     ConectionImg1: "/images/h1.png",
     GirlImg2: "/images/h2.png",
     Logo: "/images/logo.svg",
+    LogoBGBlue: "/images/logo_login.svg",
     LogoFooter: "/images/logo_footer.svg",
     LogoUserFooter: "/images/logo-epa-3.svg",
     Img1: "/images/f1.png",
@@ -9,11 +10,11 @@ const ImgPath = {
     Img3: "/images/f3.png",
     ImgCoracao: "/images/iconeCoracao.png",
     ImgJeff: "/images/imgJeff.png",
-    ImgBreno: "/images/imgBreno.png",
+    ImgBreno: "/images/imgBreno.jpg",
     ImgJorge: "/images/imgJorge.png",
     ImgThiago: "/images/imgThiago.png",
     ImgCaio: "/images/imgCaio.png",
-    
+
 }
 
 export default ImgPath;
