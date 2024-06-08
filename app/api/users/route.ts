e7e76d264cd2e1671c57d356defb1a7f99895fc3
Antônio Thiago Neto <atn.coding@gmail.com>
2024-06-08 18:00:05 -0300

    fix build to deploy

--- app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-// import { NextResponse } from "next/server";
-// import data from '@/data.json'
-
-
-// export async function GET() {
-//     return NextResponse.json(
-//         { data, }
-//     )
-// }
